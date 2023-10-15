f4290bc05fa02f0c7c7c7e0fc3aba78bba4d709a
sa lithish <118324814+lithish987@users.noreply.github.com>
2023-10-15 17:15:17 +0530

    ardino.c

--- rdofile.c
+++ rdofile.c
@@ -16,7 +16,7 @@ Servo myservo;
 #define ir_car6 10
 
 int S1=0, S2=0, S3=0, S4=0, S5=0, S6=0;
-int flag1=0, flag2=0, exitCount=0; 
+int flag1=0, flag2=0; 
 int slot = 6;  
 
 void setup(){
@@ -79,35 +79,39 @@ if(S5==1){lcd.print("S5:Fill ");}
      else{lcd.print("S5:Empty");}
 
 lcd.setCursor (10,3);
-if(S6==1){
-  lcd.print("S6:Fill ");
-}
-else{
-  lcd.print("S6:Empty");
-}    
+if(S6==1){lcd.print("S6:Fill ");}
+     else{lcd.print("S6:Empty");}    
 
 if(digitalRead (ir_enter) == 0 && flag1==0){
-  if(slot>0){
-    flag1=1;
-    if(flag2==0){
-      myservo.write(180); slot = slot-1;
-    }
-  }
-  else{
-    lcd.setCursor (0,0);
-    lcd.print(" Sorry Parking Full ");  
-    delay(1500);
-  }   
+if(slot>0){flag1=1;
+if(flag2==0){myservo.write(180); slot = slot-1;}
+}else{
+lcd.setCursor (0,0);
+lcd.print(" Sorry Parking Full ");  
+delay(1500);
+}   
 }
 
-if(digitalRead (ir_back) == 0 && flag2==0){
-  flag2=1;
-  exitCount++;
-  if(exitCount == 2) { // Check if two cars have exited
-    if(flag1==0){
-      myservo.write(180); 
-      slot = slot+1;
-    }
-    exitCount = 0; // Reset exit count flag
-  }
+if(digitalRead (ir_back) == 0 && flag2==0){flag2=1;
+if(flag1==0){myservo.write(180); slot = slot+1;}
+}
+
+if(flag1==1 && flag2==1){
+delay (1000);
+myservo.write(90);
+flag1=0, flag2=0;
+}
+
+delay(1);
+}
+
+void Read_Sensor(){
+S1=0, S2=0, S3=0, S4=0, S5=0, S6=0;
+
+if(digitalRead(ir_car1) == 0){S1=1;}
+if(digitalRead(ir_car2) == 0){S2=1;}
+if(digitalRead(ir_car3) == 0){S3=1;}
+if(digitalRead(ir_car4) == 0){S4=1;}
+if(digitalRead(ir_car5) == 0){S5=1;}
+if(digitalRead(ir_car6) == 0){S6=1;}  
 }
